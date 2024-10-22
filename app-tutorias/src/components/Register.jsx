8b9eb8c51c31a3fd4829f1efb80703ed1e4783be
Nerby16 <bryanenriquesalazarluna@gmail.com>
2024-10-22 12:06:19 +0200

    Register casi hecho

--- app-tutorias/src/components/Register.jsx
+++ app-tutorias/src/components/Register.jsx
@@ -1,26 +1,65 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import logo from '../images/InterfazHome/logo-universae-linktree.webp'
+import { supabase } from './supabase/SupabaseCliente'
+import { useNavigate } from 'react-router-dom';
+
 
 const Register = () =>{
 
+    const navigate = useNavigate();  // Hook para navegar entre páginas
     const [Username, setUsername]= useState('');
     const [Name, setName]= useState('');
-    const [ID, setId]= useState('');
     const [Surname,setSurname] =useState('');
     const [Email, setEmail]=useState('');
     const [Password, setPassword]=useState('');
     const [ConfirmPassword, setConfirmPassword]=useState('');
     const [passwordError, setPasswordError] = useState('');
     const [Grade, setGrade]=useState('');
-    const navigate = useNavigate();  // Hook para navegar entre páginas
+    
     const handleRegister = async (e) => {
         e.preventDefault();
+        
+
+        if (Password !== ConfirmPassword) {
+            setPasswordError("Las contraseñas no coinciden");
+            return;
+        }
         try{
+            const {data, error} = await supabase.auth.signUp({
+                email: Email,
+                password : Password,
+            });
+                ;
+            if (error) {
+                console.error("Error al registrar el usuario:", error);
+            } else {
+                const user = supabase.auth.user();
+                const {error: insertError} = await supabase
+                    .from('usuario')
+                    .insert([
+                        {
+                            id: user.id,
+                            username: Username,
+                            name: Name,
+                            surname: Surname,
+                            email: Email,
+                            grade: Grade,
+                        },
+                    ]);
+                if (insertError) {
+                    console.error('Error al insertar datos adicionales del usuario:', insertError);
+                    } else {
+                    // Redirigir al usuario a la página de login después del registro exitoso
+                    navigate('/login');
+            }
+        }
+                
+
+
 
         } catch (err)
         {
-
+            console.error("Error inesperado:", err);
         }
 
 
@@ -41,7 +80,7 @@ const Register = () =>{
             <div className="w-full max-w-md p-8 my-8 space-y-6 bg-white rounded-lg shadow-lg">
                 <h2 className="text-2xl font-bold text-center w-32 h-32"><img src={logo} alt="Logo" /></h2>
                 <form className="space-y-4" onSubmit={handleRegister}>
-                    <div class= "grid md:grid-cols-2 md:gap-6">
+                    <div className= "grid md:grid-cols-2 md:gap-6">
                             <div>
                                 <label htmlFor="name" className="block text-sm font-medium text-gray-700">
                                 Nombre 
@@ -77,15 +116,14 @@ const Register = () =>{
                                     id="mail"
                                     name="mail"
                                     type="email"
-                                    placeholder="example@gmail.com"
-                                    pattern=".+@Universae\.com"
+                                    placeholder="example@email.com"
                                     required
                                     value={Email}
                                     onChange={(e)=> setEmail(e.target.value)}
                                     className="w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500"/>
                         
                     </div>
-                    <div class="grid md:grid-cols-2 md:gap-6">
+                    <div className="grid md:grid-cols-2 md:gap-6">
                                 <div>
                                     
                                     <label htmlFor="UserName" className="block text-sm font-medium text-gray-700">
@@ -104,13 +142,15 @@ const Register = () =>{
                                     <label htmlFor="grade" className="block text-sm font-medium text-gray-700">
                                     Grado
                                     </label>
-                                    <select className="w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500">
-                                        <option value="default"> Opcion 1</option>
-                                        <option value="">Opcion 2</option>
+                                    <select className="w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500" 
+                                    value={Grade}
+                                    onChange={(e) => setGrade(e.target.value)}>
+                                        <option value="default"> DAM</option>
+                                        <option value="">DAW</option>
                                     </select>
                                 </div>
                     </div>
-                    <div class="grid md:grid-cols-2 md:gap-6">
+                    <div className="grid md:grid-cols-2 md:gap-6">
                             <div>
                                 <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                                     Contraseña
