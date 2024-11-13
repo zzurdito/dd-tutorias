1aed00eb614d753098d9ea21154298b256d7e18e
Alejandro Gomez <128636621+zzurdito@users.noreply.github.com>
2024-11-13 10:40:59 +0100

    Delete app-tutorias/app-tutorias directory

--- app-tutorias/app-tutorias/src/components/Events.jsx
+++
@@ -1,9 +0,0 @@
-
-
-function Events() {
-  return (
-    <div>Events</div>
-  )
-}
-
-export default Events
--- app-tutorias/app-tutorias/src/components/supabase/SupabaseCliente.jsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://itiwanyeuidisxsegfsn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0aXdhbnlldWlkaXN4c2VnZnNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk0OTc5MDQsImV4cCI6MjA0NTA3MzkwNH0.3VBHA6aaL0-SkqzIvh8_yrbDRz6t_3olxxce0CsJEoc';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
